a7acbf761ba825148f0a19f91643f9def532c310
DieselTank <dnshelby712@gmail.com>
2017-04-16 16:11:33 -0500

    initial commit

---
+++ Nutrition App/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Nutrition_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtbxSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblResults = new System.Windows.Forms.Label();
+            this.txtbxResults = new System.Windows.Forms.TextBox();
+            this.txtbxAdd = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtbxCarbs = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtbxSearch
+            // 
+            this.txtbxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbxSearch.Location = new System.Drawing.Point(12, 12);
+            this.txtbxSearch.Name = "txtbxSearch";
+            this.txtbxSearch.Size = new System.Drawing.Size(383, 49);
+            this.txtbxSearch.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(427, 12);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(153, 49);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lblResults
+            // 
+            this.lblResults.AutoSize = true;
+            this.lblResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResults.Location = new System.Drawing.Point(12, 202);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(153, 42);
+            this.lblResults.TabIndex = 6;
+            this.lblResults.Text = "Results:";
+            // 
+            // txtbxResults
+            // 
+            this.txtbxResults.Location = new System.Drawing.Point(12, 247);
+            this.txtbxResults.Multiline = true;
+            this.txtbxResults.Name = "txtbxResults";
+            this.txtbxResults.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtbxResults.Size = new System.Drawing.Size(383, 523);
+            this.txtbxResults.TabIndex = 5;
+            // 
+            // txtbxAdd
+            // 
+            this.txtbxAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbxAdd.Location = new System.Drawing.Point(12, 94);
+            this.txtbxAdd.Name = "txtbxAdd";
+            this.txtbxAdd.Size = new System.Drawing.Size(378, 49);
+            this.txtbxAdd.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(624, 94);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(153, 49);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtbxCarbs
+            // 
+            this.txtbxCarbs.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbxCarbs.Location = new System.Drawing.Point(427, 97);
+            this.txtbxCarbs.Name = "txtbxCarbs";
+            this.txtbxCarbs.Size = new System.Drawing.Size(153, 49);
+            this.txtbxCarbs.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 792);
+            this.Controls.Add(this.txtbxCarbs);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtbxAdd);
+            this.Controls.Add(this.txtbxResults);
+            this.Controls.Add(this.lblResults);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtbxSearch);
+            this.Name = "Form1";
+            this.Text = "Nutrition Tracker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtbxSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblResults;
+        private System.Windows.Forms.TextBox txtbxResults;
+        private System.Windows.Forms.TextBox txtbxAdd;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtbxCarbs;
+    }
+}
+
---
+++ Nutrition App/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Nutrition_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\Users\dnshe\Documents\Nutrition.txt";
+            if (File.Exists(path))
+            {
+                StreamReader sr = new StreamReader(path);
+                string itemName = txtbxSearch.Text;
+                List<string> itemNames = new List<string>();
+                if (itemName == "all")
+                {
+                    foreach (string line in File.ReadAllLines(path))
+                    {
+                        itemNames.Add(line);
+                    }
+                }
+                else
+                {
+                    foreach (string line in File.ReadAllLines(path))
+                    {
+                        if (line.Contains(itemName))
+                        {
+                            itemNames.Add(line);
+                        }
+                    }
+
+                }
+
+                txtbxResults.Text = itemNames.Count + " item(s) found!\r\n\r\n";
+
+                foreach (object o in itemNames)
+                {
+                    txtbxResults.Text += o.ToString() + "\r\n";
+                }
+
+                sr.Close();
+            }
+
+            else
+            {
+                MessageBox.Show("No file found!\r\n\r\nAdd an entry to create a new file.", "Error");
+            }
+        } 
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\Users\dnshe\Documents\Nutrition.txt";
+            StreamWriter sw;
+
+            string itemName = txtbxAdd.Text;
+            string carbs = txtbxCarbs.Text;
+
+            if (!File.Exists(path))
+            {
+                using (sw = File.CreateText(path))
+                {
+                    sw.WriteLine(itemName + " " + carbs);
+                }
+            }
+            else
+            {
+                using (sw = File.AppendText(path))
+                {
+                    sw.WriteLine(itemName + " " + carbs);
+                }
+            }
+
+            sw.Close();
+        }
+    }
+}
